2a2682b3057dfea13a828c60b02a102f644eb6d0
Adam A.G. Shamblin <adam.shamblin@gmail.com>
2016-07-01 14:08:29 -0600

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='web2048',
+    version='0.1',
+    description='Demo py2048 online.',
+    author='Adam A.G. Shamblin',
+    author_email='adam.shamblin@tutanota.com',
+    license='MIT',
+    install_requires=[
+        'tornado>=4'
+    ])
